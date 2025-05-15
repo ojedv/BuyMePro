3cf9e1018b29ff09413384d1ddc075a35f787169
Miguel Moreno <mmorgalm@alumnos.ilerna.com>
2025-05-15 16:52:15 +0200

    Añadida la clase Producto procedente de la tabla producto de la base de datos

---
+++ Aplication/src/Producto.java
@@ -0,0 +1,58 @@
+public class Producto {
+    private int idProducto;
+    private String nombre;
+    private float precio;
+    private float peso;
+    private int idSupermercado;
+
+    // Constructor vacío
+    public Producto() {}
+
+    // Constructor con todos los campos
+    public Producto(String nombre, float precio, float peso, int idSupermercado) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.peso = peso;
+        this.idSupermercado = idSupermercado;
+    }
+
+    // Getters y Setters
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public int getIdSupermercado() {
+        return idSupermercado;
+    }
+
+    public void setIdSupermercado(int idSupermercado) {
+        this.idSupermercado = idSupermercado;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "idProducto=" + idProducto +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", peso=" + peso +
+                ", idSupermercado=" + idSupermercado +
+                '}';
+    }
+}
