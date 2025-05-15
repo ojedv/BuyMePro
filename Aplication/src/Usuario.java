758ec3098837969e0dd504c624b6cc75ca407528
ojedv <ojedafj@alumnos.ilerna.com>
2025-05-15 16:28:00 +0200

    Clase usuario

---
+++ Aplication/src/Usuario.java
@@ -0,0 +1,67 @@
+public class Usuario {
+    private int id_usuario;
+    private String nickname;
+    private String contraseña;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String correo;
+
+    public Usuario(String nickname, String contraseña, String nombre, String apellidos, String telefono, String correo) {
+        this.nickname = nickname;
+        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public int getIdBBDD(){
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id_usuario=" + id_usuario +
+                ", nickname='" + nickname + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+}
