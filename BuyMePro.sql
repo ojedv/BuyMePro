d5e9f743d00b91b0d1e7794966cfaee2eda0de77
Miguel Moreno <mmorgalm@alumnos.ilerna.com>
2025-05-22 19:24:07 +0200

    Implementación de la Interfaz Gráfica muy bien

--- BuyMePro.sql
+++ BuyMePro.sql
@@ -45,31 +45,79 @@ CREATE TABLE grupo_supermercado ( -- ESTA TABLA NOS PERMITE CONTROLAR LA RELACIO
 CREATE TABLE producto(
 id_producto INT PRIMARY KEY AUTO_INCREMENT,
 nombre VARCHAR (30),
-precio FLOAT, -- PONEMOS EL PRECIO PARA QUE APAREZCA AL SELECCIONAR EL PRODUCTO
-peso FLOAT, -- EL PESO LO PONEMOS PARA CONTROLAR QUE NO HAYA EXCESO DE PRODUCTOS/KG EN LOS PEDIDOS
-id_supermercado INT, -- CADA PRODUCTO TIENE SU SUPERMERCADO
-FOREIGN KEY (id_supermercado) REFERENCES supermercado (id_supermercado)
+precio FLOAT -- PONEMOS EL PRECIO PARA QUE APAREZCA AL SELECCIONAR EL PRODUCTO
+
+
 
 );
 
-CREATE TABLE pedido(
-id_pedido INT PRIMARY KEY AUTO_INCREMENT,
-id_usuario INT,
-FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-
-);
-
-CREATE TABLE pedido_producto(
-id_pedido INT,
+CREATE TABLE producto_supermercado (
+id_supermercado INT, 
 id_producto INT,
-cantidad INT,
-FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
-FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+FOREIGN KEY (id_supermercado) REFERENCES supermercado(id_supermercado)
 
 );
+
+INSERT INTO supermercado (nombre) 
+VALUES 
+('Mercadona'),
+('Lidl'),
+('Más'),
+('Dia');
+
+
+-- Insertar productos para Mercadona (id_supermercado = 1)
+INSERT INTO producto (nombre, precio, id_supermercado) VALUES 
+('Leche Entera 1L', 1.20, 1),
+('Pan de Molde', 1.50, 1),
+('Huevos Docena', 2.30, 1),
+('Arroz 1kg', 1.80, 1),
+('Aceite de Oliva 1L', 4.50, 1),
+('Pollo Entero', 3.20, 1),
+('Tomates 1kg', 2.10, 1),
+('Pasta Espaguetis', 1.40, 1);
+
+-- Insertar productos para Lidl (id_supermercado = 2)
+INSERT INTO producto (nombre, precio, id_supermercado) VALUES 
+('Leche Semidesnatada 1L', 1.15, 2),
+('Pan Integral', 1.60, 2),
+('Yogur Natural Pack 4', 2.00, 2),
+('Cereales 500g', 3.20, 2),
+('Jamón Cocido 200g', 2.80, 2),
+('Queso Manchego 250g', 4.20, 2),
+('Plátanos 1kg', 1.90, 2),
+('Atún Lata Pack 3', 3.50, 2);
+
+-- Insertar productos para Dia (id_supermercado = 3)
+INSERT INTO producto (nombre, precio, id_supermercado) VALUES 
+('Leche Desnatada 1L', 1.10, 3),
+('Galletas María', 1.20, 3),
+('Café Molido 250g', 2.80, 3),
+('Azúcar 1kg', 1.50, 3),
+('Mantequilla 250g', 2.40, 3),
+('Salmón Ahumado 100g', 5.20, 3),
+('Naranjas 2kg', 2.60, 3),
+('Detergente Líquido 1L', 3.80, 3);
+
+-- Insertar productos para Mas (id_supermercado = 4)
+INSERT INTO producto (nombre, precio, id_supermercado) VALUES 
+('Agua Mineral 6x1.5L', 2.40, 4),
+('Cerveza Pack 6', 4.80, 4),
+('Pizza Congelada', 3.20, 4),
+('Helado 1L', 3.80, 4),
+('Chocolate Negro 100g', 2.20, 4),
+('Patatas Fritas 150g', 1.80, 4),
+('Manzanas 1.5kg', 2.90, 4),
+('Papel Higiénico Pack 6', 4.50, 4);
+
+
+
+
 
 SELECT * FROM usuario;
 SELECT * FROM producto;
+SELECT * FROM supermercado;
 
 
 
